99b5fb6a719eca66274742d68007387e9d3a5baf
Christian Morales <christianm9916@gmail.com>
2022-01-24 14:55:29 -0600

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Christian Morales on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
